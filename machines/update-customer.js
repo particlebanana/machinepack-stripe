54aba33b729c867621abd480c6e98008e419b674
Scott Gress <scottmgress@gmail.com>
2016-08-23 11:17:39 +0800

    Add "update customer" machine

---
+++ machines/update-customer.js
@@ -0,0 +1,104 @@
+module.exports = {
+
+
+  friendlyName: 'Update customer',
+
+
+  description: 'Update a customer\'s details.',
+
+
+  sideEffects: 'idempotent',
+
+
+  inputs: {
+
+    apiKey: require('../constants/apiKey.input'),
+
+
+    customer: {
+      description: 'The ID of an existing customer to update.',
+      example: 'cus_4kmLwU2PvQBeqq',
+      required: true
+    },
+
+
+    email: {
+      description: 'An email address to associate with the customer.',
+      example: 'john.doe@somebody.com'
+    },
+
+
+    description: {
+      description: 'Arbitrary string to attach to the customer.',
+      example: 'sdjasnd928u8duasd'
+    },
+
+
+    source: {
+      friendlyName: 'Default source',
+      description: 'A token used to set the new customer\'s default card.',
+      whereToGet: {
+        url: 'https://stripe.com/docs/stripe.js',
+        description: 'Use stripe.js to create a card token.'
+      },
+      example: 'tok_18epPlAE8iIXJx4mLWRYLRj6'
+    },
+
+
+    metadata: {
+      description: 'Metadata to attach to the customer.',
+      example: {}
+    }
+
+
+  },
+
+  exits: {
+
+    success: {
+
+      outputFriendlyName: 'Updated Stripe customer',
+      outputDescription: 'Details of the newly-updated Stripe customer.',
+      outputExample: require('../constants/customer.object')
+    }
+  },
+
+  fn: function (inputs, exits) {
+
+    // Import `stripe`, and initialize it with the give API key.
+    var stripe = require('stripe')(inputs.apiKey);
+
+    // Get the base options.
+    var options = {
+      description: inputs.description
+    };
+
+    // Add the `email` option if provided.
+    if (inputs.email) {
+      options.email = inputs.email;
+    }
+
+    // Add the `source` option if provided.
+    if (inputs.source) {
+      options.source = inputs.source;
+    }
+
+    // Add the `metadata` option if provided.
+    if (inputs.metadata) {
+      options.metadata = inputs.metadata;
+    }
+
+    // Use the Stripe API to update the customer.
+    stripe.customers.update(inputs.customer, options, function(err, customer) {
+      // Send any errors through the `error` exit.
+      // TODO: handle more specific exits (i.e. rate limit, customer does not
+      // exist, etc.), possibly via a separate `negotiateError` machine.
+      if (err) return exits.error(err);
+
+      // Send information about the updated customer through the `success` exit.
+      return exits.success(customer);
+    });
+
+  }
+
+};
