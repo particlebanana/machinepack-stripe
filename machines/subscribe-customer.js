9195310201fb027df0a2d0514d8834df8c5c8b4b
Scott Gress <scottmgress@gmail.com>
2016-08-23 11:16:22 +0800

    Update "subscribe customer" machine
    Use constants, bring up to latest machine spec, and use recommended `stripe.subscriptions.create` method

--- machines/subscribe-customer.js
+++ machines/subscribe-customer.js
@@ -1,107 +1,115 @@
 module.exports = {
 
-  friendlyName: 'Subscibe Customer',
+
+  friendlyName: 'Subscribe Customer',
+
+
   description: 'Subscribe a customer to a pre-existing plan.',
+
+
   extendedDescription: 'To subscribe a customer, you must first create a customer object with a saved card and a subscription plan (You can do this in your Stripe control panel). If your API key is in test mode, the supplied card won\'t actually be charged, though everything else will occur as if in live mode. (Stripe assumes that the charge would have completed successfully).  Also note that you can use one of the [test cards provided by Stripe](https://stripe.com/docs/testing#cards), each of which always fails in one predetermined ways.',
-  cacheable: false,
+
+
+  sideEffects: 'idempotent',
+
 
   inputs: {
-    apiKey: {
-      description: 'Your Stripe API key',
-      whereToGet: {
-        url: 'https://dashboard.stripe.com/account/apikeys',
-        description: 'Copy either "Test Secret Key" or "Live Secret Key" from your Stripe dashboard.',
-        extendedDescription: 'Make sure you are logged in to your Stripe account, or create an account if you have not already done so.'
-      },
-      example: 'somestring837483749blah',
+
+    apiKey: require('../constants/apiKey.input'),
+
+    customer: {
+      description: 'The ID of an existing customer to subscribe to the plan.',
+      example: 'cus_4kmLwU2PvQBeqq',
       required: true
     },
+
     plan: {
-      description: 'The plan id to subscribe the customer to.',
+      description: 'The ID of a plan to subscribe the customer to.',
       extendedDescription: 'You can find this in your stripe control panel after creating a subscription.',
       example: 'premium',
       required: true
     },
+
+    source: {
+      description: 'The ID of a saved payment source to charge.',
+      extendedDescription: 'The ID of a payment source token.  If provided, this will become the customer\'s new default payment source.  If not provided, the customer must already have a payment source added, or else the subscription creation will fail.',
+      example: 'tok_someCardIdjsd2isnsd',
+    },
+
     quantity: {
       description: 'If using a plan with multiple users or other dynamic elements, specify the quantity.',
       example: 20,
-      required: true
     },
-    card: {
-      description: 'The Stripe id of a saved card to charge.',
-      extendedDescription: 'If you also pass a customer ID, the card must be the ID of a card belonging to the customer. Otherwise, if you do not pass a customer ID, the card you provide must either be a Stripe token, like the ones returned by Stripe.js.',
-      example: 'tok_someCardIdjsd2isnsd',
-      required: true
+
+    coupon: {
+      description: 'The code of a coupon to apply to this subscription',
+      extendedDescription: 'A coupon applied to a subscription will only affect invoices created for that particular subscription.',
+      example: 'HALFOFF'
     },
-    customer: {
-      description: 'The Stripe id of an existing customer to subscribe to the plan.',
-      example: 'cus_4kmLwU2PvQBeqq',
-      required: true
+
+    trialEnd: {
+      friendlyName: 'Trial end date',
+      description: 'Timestamp representing the end of the trial period the customer will get before being charged for the first time.',
+      extendedDescription: 'If set, trial_end will override the default trial period of the plan the customer is being subscribed to.',
+      example: 1471583633
+    },
+
+    metadata: {
+      description: 'Metadata to attach to the subscription.',
+      example: {}
     }
+
   },
 
-  defaultExit: 'success',
-
   exits: {
-    error: {
-      description: 'Unexpected error',
-      variableName: 'err'
-    },
+
     success: {
-      variableName: 'newSubscription',
-      example:    {
-        id: 'sub_6EOW9GHkrQ53Z2',
-        plan: {
-          interval: 'month',
-          name: 'Gold Plan',
-          created: 1431566401,
-          amount: 2000,
-          currency: 'gbp',
-          id: 'gold',
-          object: 'plan',
-          livemode: false,
-          interval_count: 1,
-          trial_period_days: 0,
-          metadata: {},
-          statement_descriptor: 'foo'
-        },
-        object: 'subscription',
-        start: 1431505346,
-        status: 'active',
-        customer: 'cus_654EF6eI4JFErdS',
-        cancel_at_period_end: false,
-        current_period_start: 1431507646,
-        current_period_end: 1434186046,
-        ended_at: 0,
-        trial_start: 0,
-        trial_end: 0,
-        canceled_at: 0,
-        quantity: 2,
-        application_fee_percent: 0,
-        discount: 0,
-        tax_percent: 0,
-        metadata: {}
-      }
+      outputFriendlyName: 'New Stripe subscription',
+      outputDescription: 'Details of the Stripe subscription that has been added to the customer.',
+      outputExample: require('../constants/subscription.object')
     }
   },
 
   fn: function (inputs, exits) {
 
-    // TODO: handle more specific exits (i.e. rate limit, customer does not exist, etc.)
-
+    // Import `stripe`, and initialize it with the given API key.
     var stripe = require('stripe')(inputs.apiKey);
 
-    // Get the base options
+    // Set the base options for the API request.
     var options = {
-      plan: inputs.plan,
-      quantity: inputs.quantity,
-      card: inputs.card
+      customer: inputs.customer,
+      plan: inputs.plan
     };
 
+    // Add in source if provided.
+    if (inputs.source) {
+      options.source = inputs.source;
+    }
 
-    stripe.customers.createSubscription(inputs.customer, options, function(err, charge) {
+    // Add in quantity if provided.
+    if (inputs.quantity) {
+      options.quantity = inputs.quantity;
+    }
+
+    // Add in coupon if provided.
+    if (inputs.coupon) {
+      options.coupon = inputs.coupon;
+    }
+
+    // Add in metadata if provided.
+    if (inputs.metadata) {
+      options.metadata = inputs.metadata;
+    }
+
+    // Add in trial end date if provided.
+    if (inputs.trialEnd) {
+      options.trial_end = inputs.trialEnd;
+    }
+
+    // Use the Stripe API to create the new subscription.
+    stripe.subscriptions.create(options, function(err, subscription) {
       if (err) return exits.error(err);
-      return exits.success(charge);
+      return exits.success(subscription);
     });
 
   }
