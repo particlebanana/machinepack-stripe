ffbad441358774d4127c6cf7414df100cbd8bbd2
Scott Gress <scottmgress@gmail.com>
2016-08-23 10:07:16 +0800

    Update "create customer" machine
    Use constants, bring up to latest machine spec

--- machines/create-customer.js
+++ machines/create-customer.js
@@ -1,108 +1,100 @@
 module.exports = {
 
+
   friendlyName: 'Create customer',
-  description: 'Create a new customer in Stripe',
-  cacheable: false,
+
+
+  description: 'Create a new customer in Stripe.',
+
 
   inputs: {
 
-    apiKey: {
-      description: 'Your Stripe API key',
-      whereToGet: {
-        url: 'https://dashboard.stripe.com/account/apikeys',
-        description: 'Copy either "Test Secret Key" or "Live Secret Key" from your Stripe dashboard.',
-        extendedDescription: 'Make sure you are logged in to your Stripe account, or create an account if you have not already done so.'
-      },
-      example: 'somestring837483749blah',
-      required: true
+    apiKey: require('../constants/apiKey.input'),
+
+    email: {
+      description: 'An email address to associate with the customer.',
+      example: 'john.doe@somebody.com'
     },
 
     description: {
-      description: 'Arbitrary string to attach to the customer in Stripe',
+      description: 'Arbitrary string to attach to the customer in Stripe.',
       example: 'sdjasnd928u8duasd'
+    },
+
+    source: {
+      description: 'A token used to set the new customer\'s default charging source.',
+      whereToGet: {
+        url: 'https://stripe.com/docs/stripe.js',
+        description: 'Use stripe.js to create a source token.'
+      },
+      example: 'tok_18epPlAE8iIXJx4mLWRYLRj6'
+    },
+
+    plan: {
+      description: 'The identifier of a plan to subscribe the customer to.',
+      example: 'pro'
+    },
+
+    metadata: {
+      description: 'Arbitrary metadata to attach to the new customer.',
+      example: {}
     }
 
   },
 
-  defaultExit: 'success',
-
   exits: {
 
-    error: {
-      description: 'Unexpected error',
-      variableName: 'err'
-    },
-
     success: {
-
-      description: 'New customer created',
-      variableName: 'newCustomer',
-      example: {
-        id: "cus_4kmLwU2PvQBeqq",
-        object: "customer",
-        created: 1410377376,
-        livemode: false,
-        description: "Customer for weatherforensics@wdtinc.com",
-        email: "foo@foo.com",
-        delinquent: false,
-        metadata: {
-        },
-        subscriptions: {
-          object: "list",
-          total_count: 0,
-          has_more: false,
-          url: "/v1/customers/cus_4kmLwU2PvQBeqq/subscriptions"
-        },
-        discount: 0,
-        account_balance: 0,
-        currency: "usd",
-        cards: {
-          object: "list",
-          total_count: 1,
-          has_more: false,
-          url: "/v1/customers/cus_4kmLwU2PvQBeqq/cards",
-          data: [{
-            id: "card_14bGkv2eZvKYlo2Co1ndeHIy",
-            object: "card",
-            last4: "4242",
-            brand: "Visa",
-            funding: "credit",
-            exp_month: 9,
-            exp_year: 2014,
-            fingerprint: "Xt5EWLLDS7FJjR1c",
-            country: "US",
-            name: "test",
-            address_line1: "2324 SW 137th Street",
-            address_line2: "",
-            address_city: "73170",
-            address_state: "OK",
-            address_zip: "73170",
-            address_country: "US",
-            cvc_check: "pass",
-            address_line1_check: "pass",
-            address_zip_check: "pass",
-            customer: "cus_4kmLwU2PvQBeqq"
-          }]
-        },
-        default_card: "card_14bGkv2eZvKYlo2Co1ndeHIy"
-      }
+      outputFriendlyName: 'New Stripe customer',
+      outputDescription: 'Details of the newly-created Stripe customer, including payment sources and subscriptions.',
+      outputExample: require('../constants/customer.object')
     }
   },
 
   fn: function (inputs, exits) {
 
-    // TODO: handle more specific exits (i.e. rate limit, invalid API key, etc.)
-
+    // Import `stripe`, and initialize it with the given API key.
     var stripe = require('stripe')(inputs.apiKey);
 
-    // Get the base options
-    var options = {
-      description: inputs.description
-    };
+    // Declare a var to hold options for the API call.
+    var options = {};
 
+    // Add the `email` option if provided.
+    if (inputs.email) {
+      options.email = inputs.email;
+    }
+
+    // Add the `description` option if provided.
+    if (inputs.description) {
+      options.description = inputs.description;
+    }
+
+    // Add the `plan` option if provided.
+    if (inputs.plan) {
+      options.plan = inputs.plan;
+    }
+
+    // Add the `source` option if provided.
+    if (inputs.source) {
+      options.source = inputs.source;
+    }
+
+    // Add the `metadata` option if provided.
+    if (inputs.metadata) {
+      options.metadata = inputs.metadata;
+    }
+
+    // Use the Stripe API to create the new customer.
     stripe.customers.create(options, function(err, customer) {
+
+      // Send any errors through the `error` exit.
+      // TODO: handle more specific exits (i.e. rate limit, customer does not
+      // exist, etc.), possibly via a separate `negotiateError` machine.
       if (err) return exits.error(err);
+
+      // Send the new customer object through the `success` exit.
       return exits.success(customer);
+
     });
 
   }
