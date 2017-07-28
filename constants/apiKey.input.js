c1b987fefa50c4dcb111d9a5879fabebebfb5a91
Mike McNeil <michael.r.mcneil@gmail.com>
2017-07-28 10:08:51 -0500

    Add setGlobalCredentials() method, and simplify updateAccount() and createAccount().

--- constants/apiKey.input.js
+++ constants/apiKey.input.js
@@ -1,11 +1,11 @@
 module.exports = {
   friendlyName: 'API Key',
   description: 'A valid Stripe API key (aka Secret Key).',
+  extendedDescription: 'This can be omitted if you\'ve used .setGlobalCredentials().',
   whereToGet: {
     url: 'https://dashboard.stripe.com/account/apikeys',
     description: 'Copy either "Test Secret Key" or "Live Secret Key" from your Stripe dashboard.',
     extendedDescription: 'Make sure you are logged in to your Stripe account, or create an account if you have not already done so.'
   },
   example: 'somestring837483749blah',
-  required: true
 };
