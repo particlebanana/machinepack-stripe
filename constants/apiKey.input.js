fdd75b6a40a0bfa7ed29c5c8c17c38323fbc87ef
Mike McNeil <michael.r.mcneil@gmail.com>
2017-10-13 10:33:34 -0500

    Breaking change: rename setGlobalCredentials() to setGlobalDefaults() for consistency.

--- constants/apiKey.input.js
+++ constants/apiKey.input.js
@@ -1,11 +1,12 @@
 module.exports = {
   friendlyName: 'API Key',
   description: 'A valid Stripe API key (aka Secret Key).',
-  extendedDescription: 'This can be omitted if you\'ve used .setGlobalCredentials().',
+  extendedDescription: 'This can be omitted if you\'ve used .setGlobalDefaults().',
   whereToGet: {
     url: 'https://dashboard.stripe.com/account/apikeys',
     description: 'Copy either "Test Secret Key" or "Live Secret Key" from your Stripe dashboard.',
     extendedDescription: 'Make sure you are logged in to your Stripe account, or create an account if you have not already done so.'
   },
   example: 'somestring837483749blah',
+  protect: true
 };
